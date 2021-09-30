bb6687b38a0e34c6fdf3c70e42d6928df040e0d8
yan-shuifeng <yanshuifeng@huawei.com>
2021-09-30 09:28:04 +0800

    #I4CE6A
    Signed-off-by: yan-shuifeng <yanshuifeng@huawei.com>
    Change-Id: I2a63e78f1469238ceb99c1c8d1e8cb75d5a31f80

--- frameworks/bridge/declarative_frontend/jsview/js_toggle.h
+++ frameworks/bridge/declarative_frontend/jsview/js_toggle.h
@@ -26,7 +26,7 @@ enum class ToggleType {
     BUTTON,
 };
 
-class JSToggle : public JSContainerBase {
+class JSToggle : public JSViewAbstract, public JSInteractable {
 public:
     static void JSBind(BindingTarget globalObj);
     static void Create(const JSCallbackInfo& info);
