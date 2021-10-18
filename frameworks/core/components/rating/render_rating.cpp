4012ede2aa6e15ef3f00cff7b78e0a4cda8bd5bb
wangwencong <wangwencong1@huawei.com>
2021-10-18 15:23:47 +0800

    rating
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id:  I1d7efa559d2f6f6f0a28b5a8f53f5dfbca9eac32

--- frameworks/core/components/rating/render_rating.cpp
+++ frameworks/core/components/rating/render_rating.cpp
@@ -384,9 +384,6 @@ void RenderRating::HandleTouchEvent(const Offset& updatePoint)
     if (NearEqual(newScore, drawScore_)) {
         return;
     }
-    if (onChangeRating) {
-        onChangeRating(static_cast<double>(drawScore_));
-    }
     drawScore_ = newScore;
     ConstrainScore(drawScore_, stepSize_, starNum_);
     UpdateRatingBar();
@@ -421,6 +418,9 @@ void RenderRating::FireChangeEvent()
     if (onScoreChange_) {
         std::string param = std::string(R"("change",{"rating":)").append(std::to_string(drawScore_).append("}"));
         onScoreChange_(param);
+    }
+    if (onChangeRating) {
+        onChangeRating(static_cast<double>(drawScore_));
     }
 }
 
