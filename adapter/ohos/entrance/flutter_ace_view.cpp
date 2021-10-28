eb8e2b78ec198e5dd140dd680e1f8da5f19e81a0
sunfei <sunfei.sun@huawei.com>
2021-10-28 15:04:03 +0800

    fix build of debug version
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id: I5b3c00ca0771d21366e288daf6781e317e3f09fb

--- adapter/ohos/entrance/flutter_ace_view.cpp
+++ adapter/ohos/entrance/flutter_ace_view.cpp
@@ -51,7 +51,7 @@ TouchPoint ConvertTouchEvent(OHOS::TouchEvent& touchEvent)
         case OHOS::TouchEnum::HOVER_POINTER_ENTER:
         case OHOS::TouchEnum::HOVER_POINTER_MOVE:
         case OHOS::TouchEnum::HOVER_POINTER_EXIT:
-            LOGD("ConvertTouchEvent not implement type:%{public}ld", touchEvent.GetAction());
+            LOGD("ConvertTouchEvent not implement type:%{public}d", touchEvent.GetAction());
             break;
         case OHOS::TouchEnum::PRIMARY_POINT_DOWN:
         case OHOS::TouchEnum::OTHER_POINT_DOWN:
