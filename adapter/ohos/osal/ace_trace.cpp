1b868871f38ed18cdf51ac5b48220f410a9d8656
hesu <hesu1@huawei.com>
2021-11-22 16:08:55 +0800

    modify the FinishTrae interface
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id:  I0a1d5bc15e100310a79ddc9e0d467d90a211a0cc

--- adapter/ohos/osal/ace_trace.cpp
+++ adapter/ohos/osal/ace_trace.cpp
@@ -15,13 +15,6 @@
 
 #include "base/log/ace_trace.h"
 
-#include <atomic>
-#include <mutex>
-#include <string>
-#include <vector>
-
-#include <pthread.h>
-
 #include "bytrace.h"
 
 #include "base/log/log.h"
@@ -29,53 +22,6 @@
 #include "base/utils/system_properties.h"
 
 namespace OHOS::Ace {
-namespace {
-
-pthread_key_t g_threadLocalKey;
-
-bool InitThreadLocalData()
-{
-    static std::once_flag onceFlag;
-    static bool initialized = false;
-    std::call_once(onceFlag, []() {
-        auto retVal = pthread_key_create(&g_threadLocalKey, [](void* ptr) {
-            if (ptr != nullptr) {
-                delete reinterpret_cast<std::vector<std::string>*>(ptr);
-            }
-        });
-        if (retVal == 0) {
-            initialized = true;
-        }
-    });
-    return initialized;
-}
-
-bool PushString(const std::string& str)
-{
-    auto vector = reinterpret_cast<std::vector<std::string>*>(pthread_getspecific(g_threadLocalKey));
-    if (vector == nullptr) {
-        vector = new std::vector<std::string>();
-        if (pthread_setspecific(g_threadLocalKey, vector) != 0) {
-            delete vector;
-            return false;
-        }
-    }
-    vector->emplace_back(str);
-    return true;
-}
-
-bool PopString(std::string& str)
-{
-    auto vector = reinterpret_cast<std::vector<std::string>*>(pthread_getspecific(g_threadLocalKey));
-    if (vector == nullptr || vector->empty()) {
-        return false;
-    }
-    str = std::move(vector->back());
-    vector->pop_back();
-    return true;
-}
-
-} // namespace
 
 bool AceTraceEnabled()
 {
@@ -87,32 +33,13 @@ void AceTraceBegin(const char* name)
     if (name == nullptr) {
         return;
     }
-
-    if (InitThreadLocalData()) {
-        std::string nameStr(name);
-        if (PushString(nameStr)) {
-            StartTrace(BYTRACE_TAG_ACE, nameStr);
-            UpdateTraceLabel();
-        } else {
-            LOGW("Failed push current tag name");
-        }
-    } else {
-        LOGW("Failed to initialized local thread data");
-    }
+    std::string nameStr(name);
+    StartTrace(BYTRACE_TAG_ACE, nameStr);
 }
 
 void AceTraceEnd()
 {
-    if (InitThreadLocalData()) {
-        std::string nameStr;
-        if (PopString(nameStr)) {
-            FinishTrace(BYTRACE_TAG_ACE);
-        } else {
-            LOGW("Failed pop last tag name");
-        }
-    } else {
-        LOGW("Failed to initialized local thread data");
-    }
+    FinishTrace(BYTRACE_TAG_ACE);
 }
 
 } // namespace OHOS::Ace
