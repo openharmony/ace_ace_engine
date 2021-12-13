ea2e510618cfc99a5f5b1160057b0c0ed090197b
hanfeng <hanfeng6@huawei.com>
2021-12-13 15:56:03 +0800

    fix data query crash
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id:  Ia9b1577a7ea8a8c764bc433390e7fe75adf32ad5

--- adapter/ohos/entrance/pa_engine/engine/jsi/jsi_pa_engine.h
+++ adapter/ohos/entrance/pa_engine/engine/jsi/jsi_pa_engine.h
@@ -98,7 +98,7 @@ private:
 using RdbValueBucketNewInstance = napi_value (*)(napi_env env, OHOS::NativeRdb::ValuesBucket& valueBucket);
 using RdbValueBucketGetNativeObject = OHOS::NativeRdb::ValuesBucket* (*)(napi_env env, napi_value& value);
 using RdbResultSetProxyNewInstance = napi_value (*)(napi_env env, OHOS::NativeRdb::AbsSharedResultSet* resultSet);
-using RdbResultSetProxyGetNativeObject = OHOS::NativeRdb::AbsSharedResultSet* (*)(const napi_env env,
+using RdbResultSetProxyGetNativeObject = OHOS::NativeRdb::AbsSharedResultSet* (*)(const napi_env& env,
     const napi_value& arg);
 using DataAbilityPredicatesNewInstance = napi_value (*)(
     napi_env env, OHOS::NativeRdb::DataAbilityPredicates* predicates);
