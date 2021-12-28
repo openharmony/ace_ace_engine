1813dc9869968746b9acd7f69ce67b3804b931dd
heyangbo <heyangbo1@h-partners.com>
2021-12-28 19:48:39 +0800

    TextArea
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id:  I3a5c2ada296ce55e536443dc67a0f640fb5f209e

--- frameworks/bridge/declarative_frontend/jsview/js_textarea.h
+++ frameworks/bridge/declarative_frontend/jsview/js_textarea.h
@@ -34,6 +34,7 @@ public:
     static void SetFontSize(const JSCallbackInfo& info);
     static void SetFontWeight(const std::string& value);
     static void SetTextColor(const JSCallbackInfo& info);
+    static void SetInputFilter(const JSCallbackInfo& info);
     static void SetFontStyle(int32_t value);
     static void SetFontFamily(const JSCallbackInfo& info);
     static void SetOnChange(const JSCallbackInfo& info);
