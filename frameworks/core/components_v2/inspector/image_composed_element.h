f76dba1dff215e8d5c2d56a5e0b9794c91bd815f
peter <zhuzijia@huawei.com>
2022-01-07 10:00:37 +0800

    image, progress component properties added
    Signed-off-by: peter <zhuzijia@huawei.com>
    Change-Id: Ifc5875c5bc110bfce4950d7cf8c0c53ff1ec572b

--- frameworks/core/components_v2/inspector/image_composed_element.h
+++ frameworks/core/components_v2/inspector/image_composed_element.h
@@ -40,7 +40,7 @@ public:
     std::string GetInterpolation() const;
     std::string GetRenderMode() const;
     std::string GetSourceSize() const;
-    std::string GetsyncMode() const;
+    std::string GetSyncLoad() const;
 
     AceType::IdType GetTargetTypeId() const override
     {
