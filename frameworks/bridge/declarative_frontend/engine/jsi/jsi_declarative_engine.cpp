7ad0405fa0494cf4f9730a9988d9524143bdf78f
Keerecels <lishihao4@huawei.com>
2022-01-19 16:32:10 +0800

    XComponent use nativewindow
    Signed-off-by: Keerecels <lishihao4@huawei.com>
    Change-Id: I6f2e8f9d455fc0cfa12291087ef0adbc87fb6c4a

--- frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
@@ -1055,7 +1055,6 @@ void JsiDeclarativeEngine::FireExternalEvent(const std::string& componentId, con
         LOGE("FireExternalEvent xcomponent is null.");
         return;
     }
-    auto textureId = static_cast<int64_t>(xcomponent->GetTextureId());
 
     auto container = Container::Current();
     if (!container) {
@@ -1067,7 +1066,15 @@ void JsiDeclarativeEngine::FireExternalEvent(const std::string& componentId, con
         LOGE("FireExternalEvent nativeView null");
         return;
     }
-    auto nativeWindow = const_cast<void*>(nativeView->GetNativeWindowById(textureId));
+
+    void* nativeWindow = nullptr;
+#ifdef OHOS_STANDARD_SYSTEM
+    nativeWindow = const_cast<void*>(xcomponent->GetNativeWindow());
+#else
+    auto textureId = static_cast<int64_t>(xcomponent->GetTextureId());
+    nativeWindow = const_cast<void*>(nativeView->GetNativeWindowById(textureId));
+#endif
+
     if (!nativeWindow) {
         LOGE("FireExternalEvent nativeWindow invalid");
         return;
