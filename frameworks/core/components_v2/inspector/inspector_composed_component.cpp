b3e090a1a8d14d0c8f9e6a15e4bdaa7295d37173
yaoyuchi <yaoyuchi@huawei.com>
2022-01-19 10:38:11 +0800

    add canvas to inspector
    Signed-off-by: yaoyuchi <yaoyuchi@huawei.com>

--- frameworks/core/components_v2/inspector/inspector_composed_component.cpp
+++ frameworks/core/components_v2/inspector/inspector_composed_component.cpp
@@ -190,7 +190,9 @@ const std::unordered_map<std::string, CreateElementFunc> CREATE_ELEMENT_MAP {
     { TEXTINPUT_COMPONENT_TAG,
         [](const std::string& id) {return AceType::MakeRefPtr<V2::TextInputComposedElement>(id); } },
     { SELECT_COMPONENT_TAG,
-        [](const std::string& id) {return AceType::MakeRefPtr<V2::SelectComposedElement>(id); } }
+        [](const std::string& id) {return AceType::MakeRefPtr<V2::SelectComposedElement>(id); } },
+    { CANVAS_COMPONENT_TAG,
+        [](const std::string& id) {return AceType::MakeRefPtr<V2::InspectorComposedElement>(id); } }
 };
 
 } // namespace
@@ -252,7 +254,8 @@ const std::unordered_map<std::string, std::string> COMPONENT_TAG_TO_ETS_TAG_MAP 
     { MENU_TAG, MENU_ETS_TAG },
     { TEXTAREA_COMPONENT_TAG, TEXTAREA_ETS_TAG },
     { TEXTINPUT_COMPONENT_TAG, TEXTINPUT_ETS_TAG },
-    { SELECT_COMPONENT_TAG, SELECT_ETS_TAG }
+    { SELECT_COMPONENT_TAG, SELECT_ETS_TAG },
+    { CANVAS_COMPONENT_TAG, CANVAS_ETS_TAG }
 };
 
 RefPtr<Element> InspectorComposedComponent::CreateElement()
--- frameworks/core/components_v2/inspector/inspector_constants.h
+++ frameworks/core/components_v2/inspector/inspector_constants.h
@@ -279,6 +279,10 @@ ACE_EXPORT extern const char TEXTINPUT_ETS_TAG[];
 ACE_EXPORT extern const char SELECT_COMPONENT_TAG[];
 ACE_EXPORT extern const char SELECT_ETS_TAG[];
 
+// canvas
+ACE_EXPORT extern const char CANVAS_COMPONENT_TAG[];
+ACE_EXPORT extern const char CANVAS_ETS_TAG[];
+
 } // namespace OHOS::Ace::V2
 
 #endif // FOUNDATION_ACE_FRAMEWORKS_CORE_COMPONENTS_V2_INSPECTOR_INSPECTOR_CONSTANTS_H
