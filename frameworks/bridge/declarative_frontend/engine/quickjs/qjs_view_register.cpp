59fc982d98e202929f5f4ffbd3652bcd2460f8a1
yaoyuchi <yaoyuchi@huawei.com>
2022-02-18 11:31:47 +0800

    add text_clock controller
    Signed-off-by: yaoyuchi <yaoyuchi@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/quickjs/qjs_view_register.cpp
+++ frameworks/bridge/declarative_frontend/engine/quickjs/qjs_view_register.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -884,6 +884,7 @@ void JsRegisterViews(BindingTarget globalObj)
     JSTextInput::JSBind(globalObj);
     JSTextInputController::JSBind(globalObj);
     JSTextClock::JSBind(globalObj);
+    JSTextClockController::JSBind(globalObj);
     JSMarquee::JSBind(globalObj);
 #ifdef PLUGIN_COMPONENT_SUPPORTED
     JSPlugin::JSBind(globalObj);
--- frameworks/core/components/text_clock/text_clock_component.cpp
+++ frameworks/core/components/text_clock/text_clock_component.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -49,13 +49,13 @@ std::string TextClockComponent::GetFormat() const
     return format_;
 }
 
-void TextClockComponent::SetStatus(bool status)
+void TextClockComponent::SetTextClockController(RefPtr<TextClockController> textClockController)
 {
-    isStart_ = status;
+    textClockController_ = textClockController;
 }
 
-bool TextClockComponent::GetStatus() const
+RefPtr<TextClockController> TextClockComponent::GetTextClockController() const
 {
-    return isStart_;
+    return textClockController_;
 }
 } // namespace OHOS::Ace
