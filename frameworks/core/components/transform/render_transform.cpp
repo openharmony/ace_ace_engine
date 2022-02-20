4f18972c22cac5b5ae8e1f5da80922c1a5ec6967
AliceGao <gaobinbin3@huawei.com>
2022-02-20 15:29:23 +0800

    fix bug of click effect failed
    Signed-off-by: AliceGao <gaobinbin3@huawei.com>
    Change-Id: I1d09cc7ce27464c6e9fb685f8474984ead15fe86

--- frameworks/core/components/transform/render_transform.cpp
+++ frameworks/core/components/transform/render_transform.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -418,6 +418,7 @@ void RenderTransform::PerformLayout()
     }
     SetLayoutSize(layoutSize);
     needUpdateOrigin_ = true;
+    MarkNeedSyncGeometryProperties();
 }
 
 void RenderTransform::UpdateTransformOrigin()
