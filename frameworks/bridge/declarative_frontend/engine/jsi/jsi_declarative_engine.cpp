1f8e4e2f5688505e9fa1c373592f480920b5326c
zhangbingce <zhangbingce@huawei.com>
2022-02-22 15:47:09 +0800

    update touchInfo of xcomponent in ndk
    Signed-off-by: zhangbingce <zhangbingce@huawei.com>
    Change-Id: Idc573832384401553bd8127740f8657e6197069b

--- frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
@@ -1136,7 +1136,7 @@ void JsiDeclarativeEngine::InitXComponent()
     ACE_DCHECK(engineInstance_);
 
     nativeXComponentImpl_ = AceType::MakeRefPtr<NativeXComponentImpl>();
-    nativeXComponent_ = new NativeXComponent(AceType::RawPtr(nativeXComponentImpl_));
+    nativeXComponent_ = new OH_NativeXComponent(AceType::RawPtr(nativeXComponentImpl_));
 }
 
 void JsiDeclarativeEngine::FireExternalEvent(const std::string& componentId, const uint32_t nodeId)
@@ -1184,7 +1184,7 @@ void JsiDeclarativeEngine::FireExternalEvent(const std::string& componentId, con
 
     std::string arguments;
     auto arkObjectRef = arkNativeEngine->LoadModuleByName(xcomponent->GetLibraryName(), true, arguments,
-        NATIVE_XCOMPONENT_OBJ, reinterpret_cast<void*>(nativeXComponent_));
+        OH_NATIVE_XCOMPONENT_OBJ, reinterpret_cast<void*>(nativeXComponent_));
 
     if (arkObjectRef.CheckException()) {
         LOGE("LoadModuleByName failed.");
--- frameworks/bridge/js_frontend/engine/v8/v8_xcomponent_bridge.h
+++ frameworks/bridge/js_frontend/engine/v8/v8_xcomponent_bridge.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -50,7 +50,7 @@ private:
     v8::Persistent<v8::Context> ctx_;
     v8::Isolate* isolate_ = nullptr;
 
-    NativeXComponent *nativeXComponent_ = nullptr;
+    OH_NativeXComponent *nativeXComponent_ = nullptr;
     RefPtr<NativeXComponentImpl> nativeXComponentImpl_;
 
     bool hasPluginLoaded_ = false;
