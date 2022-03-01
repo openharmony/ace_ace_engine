e5db70563d87917bf47c4f56b38bdc70aee6228c
wade <wengdi@huawei.com>
2022-03-01 14:49:28 +0800

    format fix
    Signed-off-by: wade <wengdi@huawei.com>

--- frameworks/base/ressched/ressched_report.h
+++ frameworks/base/ressched/ressched_report.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,7 @@
 
 #include <string>
 #include <unordered_map>
+
 #include "base/utils/macros.h"
 #include "base/utils/noncopyable.h"
 
