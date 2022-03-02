6c16ae71056f2e6a98d1765993722a5ae61b9e1d
yichengzhao <yichengzhao1@huawei.com>
2022-03-02 09:04:41 +0800

    IFChange:OnStateChanged fuction's paramater type is cheanged from stateEvent to bool
    Signed-off-by: yichengzhao <yichengzhao1@huawei.com>

--- adapter/ohos/osal/js_accessibility_manager.cpp
+++ adapter/ohos/osal/js_accessibility_manager.cpp
@@ -1093,10 +1093,9 @@ void JsAccessibilityManager::DeregisterInteractionOperation()
     return instance->DeregisterElementOperator(windowId);
 }
 
-void JsAccessibilityManager::JsAccessibilityStateObserver::OnStateChanged(
-    const Accessibility::AccessibilityStateEvent& stateEvent)
+void JsAccessibilityManager::JsAccessibilityStateObserver::OnStateChanged(const bool state)
 {
-    LOGI("JsAccessibilityManager::OnStateChanged changed:%{public}d", stateEvent.GetEventResult());
+    LOGI("JsAccessibilityManager::OnStateChanged changed:%{public}d", state);
     auto jsAccessibilityManager = GetHandler().Upgrade();
     if (!jsAccessibilityManager) {
         return;
@@ -1105,8 +1104,8 @@ void JsAccessibilityManager::JsAccessibilityStateObserver::OnStateChanged(
     auto context = jsAccessibilityManager->GetPipelineContext().Upgrade();
     if (context) {
         context->GetTaskExecutor()->PostTask(
-            [jsAccessibilityManager, &stateEvent]() {
-                if (stateEvent.GetEventResult()) {
+            [jsAccessibilityManager, state]() {
+                if (state) {
                     jsAccessibilityManager->RegisterInteractionOperation(jsAccessibilityManager->GetWindowId());
                 } else {
                     jsAccessibilityManager->DeregisterInteractionOperation();
