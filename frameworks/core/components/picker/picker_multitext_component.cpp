0912af0cceec7e700f5b0521ebc29063796d660d
xuzhidan <xuzhidan1@huawei.com>
2022-03-03 17:47:01 +0800

    Fix picker multi-text issue.
    Signed-off-by: xuzhidan <xuzhidan1@huawei.com>
    Change-Id: I4ba16c62a60175d064a26d23cbf3799d0a161486

--- frameworks/core/components/picker/picker_multitext_component.cpp
+++ frameworks/core/components/picker/picker_multitext_component.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -108,7 +108,7 @@ std::string PickerMultiTextComponent::GetSelectedObject(bool isColumnChange,
         }
         result.append(std::to_string(selectedIndexes_[index]));
     }
-    result.append(",\"status\":").append(std::to_string(status)).append("]}");
+    result.append("],\"status\":").append(std::to_string(status)).append("}");
     return result;
 }
 
