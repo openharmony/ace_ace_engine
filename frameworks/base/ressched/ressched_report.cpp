e1911fcb8a98bbe25ce5196ff6cd1083c6a40c45
jiangdayuan <jiangdayuan@huawei.com>
2022-03-08 20:32:56 +0800

    pick to release
    Signed-off-by: jiangdayuan <jiangdayuan@huawei.com>
    Change-Id: I838da976e193421f4bf19d4f0cc0d7141f8aa909

--- frameworks/base/ressched/ressched_report.cpp
+++ frameworks/base/ressched/ressched_report.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,8 @@
 namespace OHOS::Ace {
 namespace {
     const uint32_t RES_TYPE_CLICK_RECOGNIZE = 9;
+    const uint32_t RES_TYPE_PUSH_PAGE       = 10;
+    const uint32_t RES_TYPE_SLIDE           = 11;
 }
 
 ResSchedReport& ResSchedReport::GetInstance()
@@ -28,12 +30,20 @@ ResSchedReport& ResSchedReport::GetInstance()
 
 void ResSchedReport::ResSchedDataReport(const char* name)
 {
+    std::unordered_map<std::string, std::string> payload;
+    payload["name"] = name;
     if (reportDataFunc_ == nullptr) {
         reportDataFunc_ = LoadReportDataFunc();
     }
     if (reportDataFunc_ != nullptr) {
         if (strcmp(name, "click") == 0) {
-            reportDataFunc_(RES_TYPE_CLICK_RECOGNIZE, 0, name);
+            reportDataFunc_(RES_TYPE_CLICK_RECOGNIZE, 0, payload);
+        } else if (strcmp(name, "push_page") == 0) {
+            reportDataFunc_(RES_TYPE_PUSH_PAGE, 0, payload);
+        } else if (strcmp(name, "slide_on") == 0) {
+            reportDataFunc_(RES_TYPE_SLIDE, 1, payload);
+        } else if (strcmp(name, "slide_off") == 0) {
+            reportDataFunc_(RES_TYPE_SLIDE, 0, payload);
         }
     }
 }
