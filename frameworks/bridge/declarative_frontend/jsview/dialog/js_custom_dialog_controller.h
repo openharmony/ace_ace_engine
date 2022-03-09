441a46dac7a87030c1cbc199ef69fb3a1f115f16
caocan <1532643766@qq.com>
2022-03-09 14:49:36 +0800

    fix dialog when call open-close-open
    Signed-off-by: caocan <1532643766@qq.com>
    Change-Id: Ie3cd03ed9b62c82f82233965ce8af8932859846a

--- frameworks/bridge/declarative_frontend/jsview/dialog/js_custom_dialog_controller.h
+++ frameworks/bridge/declarative_frontend/jsview/dialog/js_custom_dialog_controller.h
@@ -35,13 +35,11 @@ public:
     void JsCloseDialog(const JSCallbackInfo& info);
 
 private:
-    void ShowDialog(const JSCallbackInfo& info);
+    void ShowDialog();
     void CloseDialog();
-    void NotifyCloseDialog();
 
     JSView* ownerView_ = nullptr;
     bool isShown_ = false;
-    int32_t closeOperationCount_ = 0;
     DialogProperties dialogProperties_;
     RefPtr<JsFunction> jsBuilderFunction_;
     RefPtr<JsFunction> jsCancelFunction_;
