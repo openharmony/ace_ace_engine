0a637aef9cd32d85434898946126d154be34ec86
peter <zhuzijia@huawei.com>
2022-03-10 09:11:11 +0800

    stepper,listItem组件极速预览功能
    Signed-off-by: peter <zhuzijia@huawei.com>
    Change-Id: I486236a2c74e79bb873c9d20e023767e1dfc6deb

--- frameworks/core/components/stepper/render_stepper_item.cpp
+++ frameworks/core/components/stepper/render_stepper_item.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,7 +27,7 @@ void RenderStepperItem::Update(const RefPtr<Component>& component)
         LOGW("stepper item component is null");
         return;
     }
-    index_ = stepperItem->GetIndex();
+    label_ = stepperItem->GetLabel();
     auto context = context_.Upgrade();
     if (!context) {
         LOGE("context is null");
