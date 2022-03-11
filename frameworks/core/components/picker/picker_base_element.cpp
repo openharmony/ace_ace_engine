20e832c422ef3ef8e1a9331c618812fdcbe479ea
peter <zhuzijia@huawei.com>
2022-03-11 20:03:53 +0800

    修改select,menu组件层级关系，修改slider组件参数和属性显示
    Signed-off-by: peter <zhuzijia@huawei.com>
    Change-Id: I2afb887e233b6e37606d177189b8d816b5489af4

--- frameworks/core/components/picker/picker_base_element.cpp
+++ frameworks/core/components/picker/picker_base_element.cpp
@@ -51,7 +51,7 @@ void PickerBaseElement::PerformBuild()
             LOGE("can not get accessibility manager or picker is null.");
         }
 #if defined(WINDOWS_PLATFORM) || defined(MAC_PLATFORM)
-        if (accessibilityManager) {
+        if (accessibilityManager && picker) {
             auto node = accessibilityManager->GetAccessibilityNodeById(picker->GetPickerBaseId());
             auto stack = picker->GetStack();
             if (node && stack) {
