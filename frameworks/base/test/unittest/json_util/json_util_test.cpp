ebdc9a29603bef460c4304f3e85fc070a0a401d3
kangchongtao <kangchongtao@huawei.com>
2022-03-19 18:49:14 +0800

    ArkUi内核审视0319
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>
    Change-Id: I6e3bdb4eb09b879d75d235ebdfdae1b3867262f1
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>

--- frameworks/base/test/unittest/json_util/json_util_test.cpp
+++ frameworks/base/test/unittest/json_util/json_util_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -36,8 +36,8 @@ class JsonUtilsTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp() const override;
+    void TearDown() const override;
 };
 
 void JsonUtilsTest::SetUpTestCase()
