c8f16bed1a5f694d15e06334e719bcf22f3d95cb
zhangxiao72 <zhangxiao72@huawei.com>
2022-03-21 20:00:19 +0800

    新模型中适配designWidth、autoDesignWidth
    Change-Id: Icd1b616e5daee08b7eda010ec72a9e3fddf11041
    Signed-off-by: zhangxiao72 <zhangxiao72@huawei.com>

--- frameworks/core/components/form/resource/form_manager_delegate.cpp
+++ frameworks/core/components/form/resource/form_manager_delegate.cpp
@@ -373,7 +373,7 @@ void FormManagerDelegate::ProcessFormUpdate(const AppExecFwk::FormJsInfo &formJs
         }
         hasCreated_ = true;
         onFormAcquiredCallback_(runningCardId_, formJsInfo.jsFormCodePath + "/", formJsInfo.formName,
-            formJsInfo.formData, formJsInfo.imageDataMap, formJsInfo.formSrc);
+            formJsInfo.formData, formJsInfo.imageDataMap, formJsInfo);
     } else {
         if (formJsInfo.formData.empty()) {
             LOGE("update form data success, but data is empty!!!");
