4017911994fb55f02789f75a65a10c01a068e03a
wangyunfang <wangyunfang3@huawei.com>
2022-03-29 16:52:17 +0800

    Signed-off-by: wangyunfang <wangyunfang3@huawei.com> Change-Id: Id790ff6fc32ce2e48c2f3bf268584fa1e563c200 Signed-off-by: wangyunfang <wangyunfang3@huawei.com>

--- frameworks/core/components/test/unittest/pattern_lock/render_pattern_lock_test.cpp
+++ frameworks/core/components/test/unittest/pattern_lock/render_pattern_lock_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -179,7 +179,7 @@ void RenderPatternLockTest::SetUpTestCase() {}
 void RenderPatternLockTest::TearDownTestCase() {}
 void RenderPatternLockTest::SetUp()
 {
-    context_= MockRenderCommon::GetMockContext();
+    context_ = MockRenderCommon::GetMockContext();
     renderPatternLock_ = AceType::MakeRefPtr<MockRenderPatternLock>();
     renderPatternLock_->Attach(context_);
 }
--- interfaces/innerkits/ui_service_manager/include/ui_service_interface.h
+++ interfaces/innerkits/ui_service_manager/include/ui_service_interface.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,6 +32,7 @@ public:
 
     IUIService() = default;
     virtual ~IUIService() = default;
+    
     /**
      * @brief Called back to OnPush
      *
