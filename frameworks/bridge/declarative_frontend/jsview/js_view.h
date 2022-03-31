c010a5d38f4799a1036b803f952fd929888a658d
yan-shuifeng <yanshuifeng@huawei.com>
2022-03-31 16:43:54 +0800

    fix the problem of LazyForEach deleted function
    Signed-off-by: yan-shuifeng <yanshuifeng@huawei.com>
    Change-Id: I6ad038d3c8f0d9a3041cdb2904f7e4010b563f4c

--- frameworks/bridge/declarative_frontend/jsview/js_view.h
+++ frameworks/bridge/declarative_frontend/jsview/js_view.h
@@ -114,6 +114,18 @@ public:
         jsViewFunction_->ExecuteUpdateWithValueParams(jsonData);
     }
 
+    void MarkLazyForEachProcess(const std::string& groudId)
+    {
+        isLazyForEachProcessed_ = true;
+        lazyItemGroupId_ = groudId;
+    }
+
+    void ResetLazyForEachProcess()
+    {
+        isLazyForEachProcessed_ = false;
+        lazyItemGroupId_ = "";
+    }
+
     /**
      * New CustomView child will be added to the map.
      * and it can be reterieved for recycling in next render function
@@ -153,6 +165,8 @@ private:
     WeakPtr<OHOS::Ace::ComposedElement> element_ = nullptr;
     bool needsUpdate_ = false;
     bool isStatic_ = false;
+    bool isLazyForEachProcessed_ = false;
+    std::string lazyItemGroupId_;
 
     RefPtr<ViewFunctions> jsViewFunction_;
 
@@ -161,6 +175,15 @@ private:
     // hold handle to the native and javascript object to keep them alive
     // until they are abandoned
     std::unordered_map<std::string, JSRef<JSObject>> customViewChildren_;
+
+    // hold handle to the native and javascript object to keep them alive
+    // until they are abandoned used by lazyForEach
+    std::unordered_map<std::string, JSRef<JSObject>> customViewChildrenWithLazy_;
+
+    // hold js view ids by lazy item ground.
+    // until they are abandoned used by lazyForEach
+    std::unordered_map<std::string, std::list<std::string>> lazyItemGroups_;
+
     // a set of valid viewids on a renderfuntion excution
     // its cleared after cleaning up the abandoned child.
     std::unordered_set<std::string> lastAccessedViewIds_;
