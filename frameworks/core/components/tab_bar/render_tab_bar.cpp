48e42addbe0642365df29b3f4383d2b7cd5c424a
wzztoone <wangzezhen@huawei.com>
2022-04-06 17:00:31 +0800

    fix tabbar
    Signed-off-by: wzztoone <wangzezhen@huawei.com>
    Change-Id: I2dd067a603892d3b283d92704cf701c588186cd7

--- frameworks/core/components/tab_bar/render_tab_bar.cpp
+++ frameworks/core/components/tab_bar/render_tab_bar.cpp
@@ -570,7 +570,8 @@ Offset RenderTabBar::MakeIndicatorOffset(const RefPtr<RenderNode>& item) const
             indicator_->GetLayoutSize(), Alignment::CENTER);
     }
     if (indicatorStyle_ == TabBarIndicatorStyle::DEFAULT &&
-        (!onFocused_ || SystemProperties::GetDeviceType() == DeviceType::PHONE)) {
+        (!onFocused_ || SystemProperties::GetDeviceType() == DeviceType::PHONE ||
+        SystemProperties::GetDeviceType() == DeviceType::TABLET)) {
         Size childSize = GetTabItemChildLayoutSize(item);
         offset +=
             Offset(0.0, childSize.Height() / DOUBLE_FACTOR) + indicatorPadding_.GetOffsetInPx(context->GetDipScale());
