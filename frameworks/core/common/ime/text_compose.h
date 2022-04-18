f06711ef2481691d46c0b8eaacb0b9786c0b1206
吴焕隆 <wuhl55@midea.com>
2022-04-18 10:41:26 +0800

    flutter/BUILD.gn无用注释删除，等其它PR评论问题修改
    Signed-off-by: 吴焕隆 <wuhl55@midea.com>

--- frameworks/core/common/ime/text_compose.h
+++ frameworks/core/common/ime/text_compose.h
@@ -17,6 +17,7 @@
 #define FOUNDATION_ACE_FRAMEWORKS_CORE_COMMON_IME_TEXT_COMPOSE_H
 
 #include <cstdint>
+
 #include "core/common/ime/constant.h"
 
 namespace OHOS::Ace {
@@ -24,53 +25,53 @@ namespace OHOS::Ace {
  * Stands for what text is selected and how the directional is.
  * We use base/extent to indicate the start/end position because of uncertain direction.
  */
-    struct TextCompose {
-        TextCompose() = default;
-        TextCompose(int32_t base, int32_t extent) : baseOffset(base), extentOffset(extent) {}
-        void Update(int32_t base, int32_t extent)
-        {
-            baseOffset = base;
-            extentOffset = extent;
-        }
-        // Usually called when none is selected.
-        void Update(int32_t both)
-        {
-            baseOffset = both;
-            extentOffset = both;
-        }
-        bool operator==(const TextCompose& other) const
-        {
-            return baseOffset == other.baseOffset && extentOffset == other.extentOffset;
-        }
-        bool operator!=(const TextCompose& other) const
-        {
-            return !operator==(other);
-        }
-        /**
-         * @brief Get first selected character in the text.
-         */
-        inline int32_t GetStart() const
-        {
-            return std::min(baseOffset, extentOffset);
-        }
-        /**
-         * @brief Get last selected character in the text.
-         */
-        inline int32_t GetEnd() const
-        {
-            return std::max(baseOffset, extentOffset);
-        }
-        /**
-         * @brief Whether selection is valid.
-         */
-        inline bool IsValid() const
-        {
-            return baseOffset > -1 && extentOffset > -1;
-        }
-        // May larger than, smaller than or equal to extentOffset.
-        int32_t baseOffset = -1;
-        // When paints caret, this is where the caret position is.
-        int32_t extentOffset = -1;
-    };
+struct TextCompose {
+    TextCompose() = default;
+    TextCompose(int32_t base, int32_t extent) : baseOffset(base), extentOffset(extent) {}
+    void Update(int32_t base, int32_t extent)
+    {
+        baseOffset = base;
+        extentOffset = extent;
+    }
+    // Usually called when none is selected.
+    void Update(int32_t both)
+    {
+        baseOffset = both;
+        extentOffset = both;
+    }
+    bool operator==(const TextCompose& other) const
+    {
+        return baseOffset == other.baseOffset && extentOffset == other.extentOffset;
+    }
+    bool operator!=(const TextCompose& other) const
+    {
+        return !operator==(other);
+    }
+    /**
+     * @brief Get first selected character in the text.
+     */
+    inline int32_t GetStart() const
+    {
+        return std::min(baseOffset, extentOffset);
+    }
+    /**
+     * @brief Get last selected character in the text.
+     */
+    inline int32_t GetEnd() const
+    {
+        return std::max(baseOffset, extentOffset);
+    }
+    /**
+     * @brief Whether selection is valid.
+     */
+    inline bool IsValid() const
+    {
+        return baseOffset > -1 && extentOffset > -1;
+    }
+    // May larger than, smaller than or equal to extentOffset.
+    int32_t baseOffset = -1;
+    // When paints caret, this is where the caret position is.
+    int32_t extentOffset = -1;
+};
 } // namespace OHOS::Ace
 #endif // FOUNDATION_ACE_FRAMEWORKS_CORE_COMMON_IME_TEXT_COMPOSE_H
